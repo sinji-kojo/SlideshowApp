2848bfe943a279efabc39802c9455a0c2f0979ec
shinji_kojyou <shinji_kojyou@jrass.jp>
2021-03-28 16:53:07 +0900

    20210328 16:52 Update

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,36 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var slideimage: UIImageView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
     
     // スライドショーに使用するインデックスを宣言
-     var nowIndex:Int = 0
-     
+    var nowIndex:Int = 0
+    
     // スライドショーに使用するタイマーを宣言
-     var timer: Timer!
-     
-     // スライドショーさせる画像の配列を宣言
-     var imageArray:[UIImage] = [
-         UIImage(named: "A")!,
-         UIImage(named: "B")!,
-         UIImage(named: "C")!
-     ]
-
+    var timer: Timer!
+    
+    // スライドショーさせる画像の配列を宣言
+    var imageArray:[UIImage] = [
+        UIImage(named: "A")!,
+        UIImage(named: "B")!,
+        UIImage(named: "C")!,
+        UIImage(named: "D")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        slideimage.image = imageArray[nowIndex]
     }
     
     // 画像を切り替えるセクション
-      @objc func changeImage() {
-          // indexを増やして表示する画像を切り替える
-          nowIndex += 1
-          
-          // indexが表示予定の画像の数と同じ場合
-          if (nowIndex == imageArray.count) {
-              // indexを一番最初の数字に戻す
-              nowIndex = 0
-          }
-          // indexの画像をstoryboardの画像にセットする
-          slideimage.image = imageArray[nowIndex]
+    @objc func changeImage() {
+        // indexを増やして表示する画像を切り替える
+        nowIndex += 1
+        
+        // indexが表示予定の画像の数と同じ場合
+        if (nowIndex == imageArray.count) {
+            // indexを一番最初の数字に戻す
+            nowIndex = 0
+        }
+        // indexの画像をstoryboardの画像にセットする
+        slideimage.image = imageArray[nowIndex]
     }
     @IBAction func nextButton(_ sender: Any) {
         nowIndex += 1
@@ -66,27 +70,28 @@ class ViewController: UIViewController {
         }
         // indexの画像をstoryboardの画像にセットする
         slideimage.image = imageArray[nowIndex]
-
+        
     }
     @IBAction func runstopButton(_ sender: AnyObject) {
         // 再生中か停止しているかを判定
-         if (timer == nil) {
+        if (timer == nil) {
             // タイマーをセットする
-             timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
-         } else {
-             // タイマーを停止する
-             timer.invalidate()
-             // タイマーを削除しておく(timer.invalidateだけだとtimerがnilにならないため)
-             timer = nil
-         }
-            if ( timer == nil ) {
-                nextButton.isEnable = false
-                backButton.isEnable = false }
-            else {
-                nextButton.isEnable = true
-                backButton.isEnable = true
-            }
-     }
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            nextButton.isEnabled = false
+            backButton.isEnabled = false
+            nextButton.isHidden = true
+            backButton.isHidden = true
+        } else {
+            // タイマーを停止する
+            timer.invalidate()
+            // タイマーを削除しておく(timer.invalidateだけだとtimerがnilにならないため)
+            timer = nil
+            nextButton.isEnabled = true
+            backButton.isEnabled = true
+            nextButton.isHidden = false
+            backButton.isHidden = false
+        }
+    }
     @IBAction func onTapImage(_ sender: AnyObject) {
         // セグエを使用して画面を遷移
         performSegue(withIdentifier: "result", sender: nil)
